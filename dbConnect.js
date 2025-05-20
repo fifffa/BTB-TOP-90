025e7d640abb3ec43734adeee94a0ccacd530a2a
fifffa <147136171+fifffa@users.noreply.github.com>
2025-05-20 12:35:34 +0900

    Add files via upload

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
+
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
